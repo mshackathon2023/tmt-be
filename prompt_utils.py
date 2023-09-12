145a3d82d1a2e9f8f0cd19a1fc8fc087a82853e5
michalmar <michal.marusan@gmail.com>
2023-09-12 22:14:08 +0000

    generate assessment prompt

--- prompt_utils.py
+++ prompt_utils.py
@@ -7,7 +7,7 @@ PROMPT_summarize_map_template = """The following is a set of documents
     Helpful Answer:"""
 
 # applies to whole summary
-PROMPT_summarize_reduce_template = """You are a good high school teacher. The following is set of summaries:
+PROMPT_summarize_reduce_template = """You are a teacher. Your task is to summarize documents to be understandable. The following is set of summaries:
     {doc_summaries}
     Take these and distill it into a final, consolidated summary of the main themes. 
     Helpful Answer:"""
@@ -19,3 +19,36 @@ PROMPT_title_template = """Generate short title based on the text below. Short t
     {docs}
      
     Helpful Title:"""
+
+# prompt to generate assesment quiz from give text
+PROMPT_generate_assesment = """Generate a single choice quiz from the text below. 
+
+Text:
+{text}
+
+Follow the rules when generating quiz:
+- Quiz should contain 5 questions. 
+- Answers shoud be more than single word.
+- Questions should match high school knowledge level.
+- Questions should spark curiosity.
+- Questions should not cherry-pick detatils.
+- Questions should cover important facts in the test.
+- Questions should cover general knowledge. 
+- Questions **must** be formated as validlist of JSON documents representing each questions.
+- Append a list of correct answer for each question.
+
+==> Example:
+[{
+    "Q1": "Why did the Maya civilization decline by about 900 CE ?",
+    "choices": [
+        {"A": "They were invaded by outsiders"},
+        {"B": "They abandoned their large population centers"},
+        {"C": "They were hit with a deadly virus"},
+        {"D": "They lost the ability to grow crops"},
+    ],
+    "Answer": "B"
+}]
+<== End of Example
+
+Questions:
+"""

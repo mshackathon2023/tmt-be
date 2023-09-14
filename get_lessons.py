537425c8b71b85600d3ffcfae5519fb48890ffa6
tkubica12 <tkubica12@gmail.com>
2023-09-14 06:06:14 +0200

    lesson api

---
+++ get_lessons.py
@@ -0,0 +1,46 @@
+import logging
+import azure.functions as func
+import uuid
+import json
+from azure.cosmos import CosmosClient, PartitionKey
+import azure.cosmos.exceptions as cosmos_exceptions
+import os
+
+GetLessons = func.Blueprint()
+
+@GetLessons.route(route="getlessons/{topic}", methods=["GET"])
+def getlessons(req: func.HttpRequest) -> func.HttpResponse:
+
+    # Get topic ID from route parameter
+    topic_id = req.route_params.get("topic")
+    logging.info(f"Getting topic {topic_id}")
+
+    # Connect to Cosmos DB
+    client = CosmosClient.from_connection_string(os.environ["COSMOSDB_CONNECTION_STRING"])
+    database = client.get_database_client("tmtdb")
+    container = database.get_container_client("topics")
+
+    # Query lessons from Cosmos DB
+    try:
+        items = list(container.query_items(
+            query="SELECT c.lessons FROM c WHERE c.id=@id",
+            parameters=[
+                {"name": "@id", "value": topic_id}
+            ],
+            enable_cross_partition_query=False
+        ))
+    except:
+        return func.HttpResponse(
+            f"Error when quering for topic {topic_id}",
+            headers = {"Content-Type": "application/json"},
+            status_code=400
+        )
+
+    # Prepare output
+    output = items[0]["lessons"]
+
+    return func.HttpResponse(
+        json.dumps(output),
+        headers = {"Content-Type": "application/json"},
+        status_code=200
+    )
